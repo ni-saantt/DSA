49a2965099dc081630d318ea07f32577f08684b1
Nishant Choudhary <Nishant.23FE10CDS00331@muj.manipal.edu>
2026-06-16 02:59:56 +0530

    just fixed syntex error

--- longest_com_prefix.py
+++ longest_com_prefix.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        first : strs[0]
+        first = strs[0]
         
-        for i in range(0,lena(first)):
+        for i in range(0,len(first)):
             for word in strs[1:]:
                 if i >= len(word) or word[i] != first[i]:
                     return first[:i]
